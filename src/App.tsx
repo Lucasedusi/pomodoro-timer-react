0f59c03e46370208a33ad4dd20373dfc59541e72
lucasedusi <lucasmto.16@hotmail.com>
2022-10-17 18:39:54 -0300

    :lipstick:feat: global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from "styled-components";
 
 import { defautTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/themes/global";
 
 export function App() {
 	return (
 		<ThemeProvider theme={defautTheme}>
 			<h1>Hello</h1>
+
+			<GlobalStyle />
 		</ThemeProvider>
 	);
 }
