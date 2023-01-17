bcccd63deba264e67589ea6ed54ea238a449eacc
lucasedusi <lucasmto.16@hotmail.com>
2023-01-17 16:09:31 -0300

    :lipstick:feat: header styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
 import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/themes/global";
+import { GlobalStyle } from "./styles/global";
 
 import { Router } from "./Router";
 
