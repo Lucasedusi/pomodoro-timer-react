c9290e1b72052df24497283897db71d3be4ce8aa
lucasedusi <lucasmto.16@hotmail.com>
2023-01-18 11:17:41 -0300

    :lipstick:feat: styling input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,8 +3,10 @@ import {
 	CountDownContainer,
 	FormContainer,
 	HomeContainer,
+	MinutesAmountInput,
 	Separator,
 	StartCountDownButton,
+	TaskInput,
 } from "./styles";
 
 export function Home() {
@@ -13,10 +15,14 @@ export function Home() {
 			<form action="">
 				<FormContainer>
 					<label htmlFor="taks">Vou trabalhar em</label>
-					<input id="task" />
+					<TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
 
 					<label htmlFor="minutesAmount">Durante</label>
-					<input type="number" id="minutesAmount" />
+					<MinutesAmountInput
+						type="number"
+						id="minutesAmount"
+						placeholder="00"
+					/>
 
 					<span>minutos.</span>
 				</FormContainer>
