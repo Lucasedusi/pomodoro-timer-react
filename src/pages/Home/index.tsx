f1cc8b97cb160190a1f88cb61b641f7ff7072d20
lucasedusi <lucasmto.16@hotmail.com>
2023-01-25 11:38:44 -0300

    :lipstick:feat: separator components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,16 +5,14 @@ import * as zod from "zod";
 import { differenceInSeconds } from "date-fns";
 
 import {
-	CountDownContainer,
-	FormContainer,
 	HomeContainer,
-	MinutesAmountInput,
-	Separator,
 	StartCountDownButton,
 	StopCountDownButton,
-	TaskInput,
 } from "./styles";
+
 import { useEffect, useState } from "react";
+import { NewCycleForm } from "./components/NewCycleForm";
+import { CountDown } from "./components/CountDown";
 
 const newCicleFormValidateSchema = zod.object({
 	task: zod.string().min(1, "Informe uma Taks"),
@@ -136,45 +134,8 @@ export function Home() {
 	return (
 		<HomeContainer>
 			<form onSubmit={handleSubmit(handleNewCreateCicle)} action="">
-				<FormContainer>
-					<label htmlFor="taks">Vou trabalhar em</label>
-					<TaskInput
-						id="task"
-						list="task-suggestions"
-						placeholder="Dê um nome para o seu projeto"
-						disabled={!!activeCycle}
-						{...register("task")}
-					/>
-
-					<datalist id="task-suggestions">
-						<option value="Projeto 01"></option>
-						<option value="Projeto 02"></option>
-						<option value="Projeto 03"></option>
-						<option value="Teste"></option>
-					</datalist>
-
-					<label htmlFor="minutesAmount">Durante</label>
-					<MinutesAmountInput
-						type="number"
-						id="minutesAmount"
-						placeholder="00"
-						step={5}
-						min={1}
-						max={60}
-						disabled={!!activeCycle}
-						{...register("minutesAmount", { valueAsNumber: true })}
-					/>
-
-					<span>minutos.</span>
-				</FormContainer>
-
-				<CountDownContainer>
-					<span>{minutes[0]}</span>
-					<span>{minutes[1]}</span>
-					<Separator>:</Separator>
-					<span>{seconds[0]}</span>
-					<span>{seconds[1]}</span>
-				</CountDownContainer>
+				<NewCycleForm />
+				<CountDown />
 
 				{activeCycle ? (
 					<StopCountDownButton onClick={handleInterruptCycle}>
